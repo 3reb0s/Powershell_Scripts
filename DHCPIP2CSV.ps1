3c2ef008df9351bd22121cdb68cac4b81d3ba41a
3reb0s <95045395+3reb0s@users.noreply.github.com>
2024-10-02 09:34:20 -0400

    Adding Scripts

---
+++ DHCPIP2CSV.ps1
@@ -0,0 +1,45 @@
+﻿
+Import-Module DhcpServer
+
+$dhcpServer = "SERVERNAME"
+
+$outputPath = "C:\PATH\4\CSV"
+
+
+$scopes = Get-DhcpServerv4Scope -ComputerName $dhcpServer
+
+$allLeases = @()
+
+# Get all scopes from the DHCP server
+$scopes = Get-DhcpServerv4Scope -ComputerName $dhcpServer
+
+# Loop through each scope
+foreach ($scope in $scopes) {
+    # Initialize variables for pagination
+    $start = 0
+    $pageSize = 100
+    $totalLeases = $pageSize
+
+    # Paginate through lease results until all leases are retrieved
+    while ($start -lt $totalLeases) {
+        # Get leases for the current scope with pagination
+        $scopeLeases = Get-DhcpServerv4Lease -ComputerName $dhcpServer -ScopeId $scope.ScopeId -StartRange $start -EndRange ($start + $pageSize - 1)
+
+        # Add leases to the array
+        $allLeases += $scopeLeases
+
+      
+        $start += $pageSize
+
+        
+        $totalLeases = $scopeLeases.Count
+    }
+}
+
+# Filter leases based on IP address starting with by IP 
+$filteredLeases = $allLeases | Where-Object { $_.IPAddress -like "172.20*" }
+
+# Export filtered leases to CSV file
+$filteredLeases | Export-Csv -Path $outputPath -NoTypeInformation
+
+Write-Host "Lease information for IP addresses starting with '172.20' has been exported to $outputPath."
---
+++ GetDHCPScopes.ps1
@@ -0,0 +1,11 @@
+﻿# Import the DHCP server module
+Import-Module DhcpServer
+
+# Define the DHCP server
+$dhcpServer = "sys-admdhcp1"
+
+# Get all DHCP scopes from the specified server
+$scopes = Get-DhcpServerv4Scope -ComputerName $dhcpServer
+
+# Display the scopes
+$scopes | Select-Object ScopeId, Name, StartRange, EndRange
